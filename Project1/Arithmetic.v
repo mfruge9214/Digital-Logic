f3767b073462c73c105dcf566e136e2b2333151d
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-02-25 16:05:42 -0700

    Update Arithmetic.v

--- Project1/Arithmetic.v
+++ Project1/Arithmetic.v
@@ -1,4 +1,4 @@
-module FullAdder(x, y, s, ci, co) 
+module FullAdder(x, y, s, ci, co);
 	input x, y, ci;
 	output s, co;
 	assign s=x^y^ci;
@@ -7,7 +7,7 @@ module FullAdder(x, y, s, ci, co)
 	
 endmodule 
 
-module Adder(x[3:0], y[3:0], s[3:0], c)
+		module Adder(x[3:0], y[3:0], s[3:0], c);
 output s[3:0];
 wire carry[3:0];
 assign carry[0] = 0;
@@ -18,3 +18,37 @@ for(i=0; i<4; i++)
 endgenerate 
 c = carry[3];
 endmodule 
+
+	module subtractor([3:0] A, [3:0] B, [3:0] out);
+input A, B;
+output out;
+genvar i=0;
+	for(i=0; i<4; i=i+1)
+    if B[i]==0
+        B[i]=1;
+    else
+        B[i]=0;
+	end
+B=B+1;
+FullAdder(A, B, out)
+endmodule
+	
+module mult_2([7:0] in, [7:0] out, carry):
+	input in;
+	output out, carry;
+	assign carry=0;
+	if in[7]==1
+		assign carry=1;
+	end
+	assign out= in<<1;
+endmodule
+	
+	module div_2([7:0] in, [7:0] out, remain):
+		input in;
+		output out, remain;
+		assign remain=0;
+		if in[0]==1;
+			assign remain=1;
+		end
+		assign out= in>>1;
+	endmodule
