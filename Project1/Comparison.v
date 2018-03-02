b0966d47517babaa49111a4f038071103fce009f
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-03-01 17:19:21 -0700

    Update Comparison.v

--- Project1/Comparison.v
+++ Project1/Comparison.v
@@ -1,13 +1,28 @@
-//module Master C
+module MasterC(
+	input [3:0] X,
+	input [3:0] Y,
+	input [1:0] select,
+	output [7:0] out
+	);
+	
+	wire [7:0] sigbit1, sigbit2, sigbit3, sigbit4;
+	
+	Equalmod(X, Y, sigbit1);
+	Greatermod(X, Y, sigbit2);
+	LessThanmod(X, Y, sigbit3);
+	Maxmod(X, Y, sigbit4);
+	
+	mux comparedisplay(sigbit1, sigbit2, sigbit3, sigbit4, select, out);
+endmodule
 
-
-module Equal(
+module Equalmod(
 	input [3:0] x,
 	input [3:0] y,
-	output [3:0] out
+	output [7:0] out
 	);
 	
 	reg equal;
+	
 	always @(x | y)
 	begin
 		if (x==y)
@@ -19,36 +34,37 @@ module Equal(
 			equal =0;
 		end
 	end
-	assign out[0]=equal;
+	//assign out[0]=equal;
+	assign out[0] = equal;
 endmodule 
 
 
 
-module Greater(
+module Greatermod(
 	input [3:0] x,
 	input [3:0] y,
-	output [3:0] out
+	output [7:0] out
 	);
 	
 	reg equal;
-	always @(x | y)
+	always @(x or y)
 	begin
 		if (x>y)
 		begin
-			equal =1;
+			equal = 1;
 		end
 		else 
 		begin
-			equal =0;
+			equal = 0;
 		end
 	end
 	assign out[0]=equal;
 endmodule 
 
-module LessThan(
+module LessThanmod(
 	input [3:0] x,
 	input [3:0] y,
-	output [3:0] out
+	output [7:0] out
 	);
 	
 	reg equal;
@@ -68,43 +84,49 @@ endmodule
 
 
 
-module Max(
+module Maxmod(
 	input [3:0] x,
 	input [3:0] y,
-	output [3:0] out
+	output reg [7:0] out
 	);
 	reg truth;
-	wire [3:0] equal;
+	reg equal;
+	
+	always @(*)
+	begin
 		case(x)
-		4'b0000 : assign truth= y[3] | y[2] | y[1] | y[0];
-		4'b0001 : assign truth= y[3] | y[2] | y[1];
-		4'b0010 : assign truth= y[3] | y[2] | (y[1]&y[0]);
-		4'b0011 : assign truth= y[3] | y[2];
-		4'b0100 : assign truth= y[3] | (y[2]&y[1]) | (y[2]&y[0]);
-		4'b0101 : assign truth= y[3] | (y[2]&y[1]);
-		4'b0110 : assign truth= y[3] | (y[2]&y[1]&y[0]);
-		4'b0111 : assign truth= y[3];
-		4'b1000 : assign truth= (y[3]&y[2]) | (y[3]&y[1]) | (y[3]&y[0]);
-		4'b1001 : assign truth= (y[3]&y[2]) | (y[3]&y[1]);
-		4'b1010 : assign truth= (y[3]&y[2]) | (y[3]&y[1]&y[0]);
-		4'b1011 : assign truth= (y[3]&y[2]);
-		4'b1100 : assign truth= (y[3]&y[2]&y[0]) | (y[3]&y[2]&y[1]);
-		4'b1101 : assign truth= (y[3]&y[2]&y[1]);
-		4'b1110 : assign truth= (y[3]&y[2]&y[1]&y[0]);
-		4'b1111 : assign truth= 0;
+
+			4'b0000 : truth <= y[3] | y[2] | y[1] | y[0];
+			4'b0001 : truth <= y[3] | y[2] | y[1];
+			4'b0010 : truth <= y[3] | y[2] | (y[1]&y[0]);
+			4'b0011 : truth <= y[3] | y[2];
+			4'b0100 : truth <= y[3] | (y[2]&y[1]) | (y[2]&y[0]);
+			4'b0101 : truth <= y[3] | (y[2]&y[1]);
+			4'b0110 : truth <= y[3] | (y[2]&y[1]&y[0]);
+			4'b0111 : truth <= y[3];
+			4'b1000 : truth <= (y[3] & y[2]) | (y[3]&y[1]) | (y[3]&y[0]);
+			4'b1001 : truth <= (y[3] & y[2]) | (y[3]&y[1]);
+			4'b1010 : truth <= (y[3] & y[2]) | (y[3]&y[1]&y[0]);
+			4'b1011 : truth <= (y[3] & y[2]);
+			4'b1100 : truth <= (y[3] & y[2] & y[0]) | (y[3]&y[2]&y[1]);
+			4'b1101 : truth <= (y[3] & y[2] & y[1]);
+			4'b1110 : truth <= (y[3] & y[2] & y[1] & y[0]);
+			4'b1111 : truth <= 0;
 		endcase
-	Equal (x,y, equal);
-	if equal[0]==1
+	
+	
+	if(truth == 1)
+		begin 
+			out = y;
+		end
+	else if(truth == 0)
 		begin
-		assign out[0]=0;
-		
-		end	
-	if truth==1
-		begin
-		assign out=y;
-		end	
+			out = x;
+		end
 	else
-		begin
-		assign out=x;
-		end		
+		begin 
+			out = 0;
+		end 
+	end
+	
 endmodule 
