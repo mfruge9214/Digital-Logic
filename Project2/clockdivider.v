8f510b9136ac7e68d3826dc32d2fe16ced46dffc
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-04-12 16:59:40 -0600

    Update clockdivider.v

--- Project2/clockdivider.v
+++ Project2/clockdivider.v
@@ -1,8 +1,5 @@
 module clockdivider(
 	input CLK,
-	input [10:0] start,
-	input state,
-	output [9:0] LED,
 	output [10:0] t
 	);
 	parameter ms=50000;
@@ -20,17 +17,9 @@ module clockdivider(
 		end
 		else
 		begin
-			to= to + 1;
+			to= to +1;
 			counter = 0;
 		end
-	end
-	
-	if (state==0)
-	begin
-		if (to == start)
-			begin
-			assign LED [9:0] = 10'b1111111111;
-			end
 	end
 	
 	assign t=to;
