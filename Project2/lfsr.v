ec9cddfbb58ed60386546d4d769343ca6f3d72dd
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-04-13 22:24:06 -0600

    Update lfsr.v

--- Project2/lfsr.v
+++ Project2/lfsr.v
@@ -1,25 +1,29 @@
 module lfsr(
 input clk,
+input  [1:0] enable,
 output [10:0] num
 );
 
 	wire feedback = num[9] ^ num[8];
-	reg [9:0] temp = num;
+	reg [10:0] temp = num;
 	always@(posedge clk)
 	begin
-		temp[0]<= feedback;
-		temp[1]<= temp[0];
-		temp[2]<= temp[1];
-		temp[3]<= temp[2];
-		temp[4]<= temp[3];
-		temp[5]<= temp[4];
-		temp[6]<= temp[5];
-		temp[7]<= temp[6];
-		temp[8]<= temp[7];
-		temp[9]<= temp[8];
+		if(enable[0])
+		begin
+			temp[0]<= feedback;
+			temp[1]<= temp[0];
+			temp[2]<= temp[1];
+			temp[3]<= temp[2];
+			temp[4]<= temp[3];
+			temp[5]<= temp[4];
+			temp[6]<= temp[5];
+			temp[7]<= temp[6];
+			temp[8]<= temp[7];
+			temp[9]<= temp[8];
+			temp[10]<= temp[9];
+		end
 	end
 
-	Compare(temp, clk, num);
-endmodule
-	
-	
+	assign num=temp;
+	//Compare(temp, clk, num);
+endmodule 
