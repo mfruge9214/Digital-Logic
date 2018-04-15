d978393f23008f02693832516d3538d3b05505b6
mfruge9214 <36678792+mfruge9214@users.noreply.github.com>
2018-04-15 01:06:45 -0600

    Update clockdivider.v

--- Project2/clockdivider.v
+++ Project2/clockdivider.v
@@ -1,32 +1,23 @@
 module Clockdivider(
-	input CLK,
-	input [1:0] enable,
-	output t
+	input in, 
+	output reg out
 	);
-	parameter ms=25000;
-	//parameter limit=start;
-	reg [15:0] counter;
-	//reg [10:0] to;
-	reg to;
-	//assign counter=0;
-	//to=11'b00000000000;
+
+	integer countTo;
+	initial out<=0;
 	
-	always @(posedge CLK)
+	always @ (posedge in) 
 	begin
-		if(enable[0])
+	countTo <= countTo + 1;
+		if(countTo == 49999)
 		begin
-			if (counter < ms)
-			begin
-				counter = counter + 1;
-			end
-			else
-			begin
-				to = ~to;
-				counter = 0;
-			end
+			countTo  <= 0;
+			out <= 1;
+		end
+		else
+		begin
+			out<=0;
 		end
 	end
-
-	assign t=to;
 	
 endmodule
